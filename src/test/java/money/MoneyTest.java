8421153e55626246fef26078b0874a4a32c9e8ef
tbenett <tom@benett.io>
2018-05-23 07:26:12 +0200

    feat(side-effect): second-cycle - dollar as value object

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,6 +1,5 @@
 package money;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +8,12 @@ public class MoneyTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
+    Dollar product = five.times(2);
 
-     assertEquals(10, five.amount);
+     assertEquals(10, product.amount);
+
+     product = five.times(3);
+
+     assertEquals(15, product.amount);
   }
 }
